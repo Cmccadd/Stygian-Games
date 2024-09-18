3868846f601898e2313fb08d85f920fd4df925f6
AndrewJMaier <amaier@mail.bradley.edu>
2024-09-18 09:53:21 -0500

    Got Cranks working
    Cranks will detect if the player is on/in the trigger area. When they leave, the gate will lower slowly when they leave

--- Assets/Scripts/LeverController.cs
+++ Assets/Scripts/LeverController.cs
@@ -29,32 +29,32 @@ public class LeverController : Interactable
             //switchSound.Play();
         }
     }
-    void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            intIcon.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                if (toggle == true)
-                {
-                    lightOn.SetActive(true);
-                    lightOff.SetActive(false);
-                    switchOn.SetActive(true);
-                    switchOff.SetActive(false);
-                    //switchSound.Play();
-                }
-                if (toggle == false)
-                {
-                    lightOn.SetActive(false);
-                    lightOff.SetActive(true);
-                    switchOn.SetActive(false);
-                    switchOff.SetActive(true);
-                    //switchSound.Play();
-                }
-            }
-        }
-    }
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        intIcon.SetActive(true);
+    //        if (Input.GetKeyDown(KeyCode.E))
+    //        {
+    //            if (toggle == true)
+    //            {
+    //                lightOn.SetActive(true);
+    //                lightOff.SetActive(false);
+    //                switchOn.SetActive(true);
+    //                switchOff.SetActive(false);
+    //                //switchSound.Play();
+    //            }
+    //            if (toggle == false)
+    //            {
+    //                lightOn.SetActive(false);
+    //                lightOff.SetActive(true);
+    //                switchOn.SetActive(false);
+    //                switchOff.SetActive(true);
+    //                //switchSound.Play();
+    //            }
+    //        }
+    //    }
+    //}
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("MainCamera"))
